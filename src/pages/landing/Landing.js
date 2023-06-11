c0664ff9b04bc663fa2848cc091d5245d8735599
wizkid31 <janhavisingh725@gmail.com>
2023-06-11 18:46:17 +0530

    ini

---
+++ src/pages/landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+//components import
+// 1- navbar.
+import Navbar from "../../components/navigation/Navbar";
+
+const Landing = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Landing;
